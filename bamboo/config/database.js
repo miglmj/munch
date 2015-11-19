8d180fb8baf81a79145a26e65c2c4b2d67bda5dd
Miguel Mejia <miguelmejiamontes@me.com>
2015-11-19 04:25:20 -0500

    scripted database creation and user schema definition. user schema updated.

---
+++ bamboo/config/database.js
@@ -0,0 +1,10 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        'host': 'localhost',
+        'user': 'testuser',
+        'password': 'testpass'
+    },
+	'database': 'bamboo',
+    'users_table': 'users'
+};
--- bamboo/scripts/create_database.js
+++ bamboo/scripts/create_database.js
@@ -13,6 +13,7 @@ connection.query('\
 CREATE TABLE `' + dbconfig.database + '`.`' + dbconfig.users_table + '` ( \
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT, \
     `username` VARCHAR(20) NOT NULL, \
+    `email` VARCHAR(30) NOT NULL, \
     `password` CHAR(60) NOT NULL, \
         PRIMARY KEY (`id`), \
     UNIQUE INDEX `id_UNIQUE` (`id` ASC), \
