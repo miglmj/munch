2c04c5e1ec4d1a9b2ad36d52d2cfa4ef45e7d324
Miguel Mejia <miguelmejiamontes@me.com>
2015-11-30 00:25:15 -0500

    begin routes and views

--- cactus/routes/index.js
+++ cactus/routes/index.js
@@ -1,5 +1,15 @@
-module.exports = function(app, passport) {
+module.exports = function(app) {
 
   // Home Page
-  
+  app.get('/', function(req, res) {
+    res.render('index');
+  });
+
+  // app.get('/login', function(req, res) {
+  //   //render the page and pass any existing flash data
+  //   res.render('login', { message: req.flash('loginMessage')});
+  // });
+
+
+
 }
--- cactus/server.js
+++ cactus/server.js
@@ -11,7 +11,13 @@ var routes = require('./routes/index');
 // call expresss constructor
 var app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap'));
+
 
 // Use routes file for all requests
 app.use('/', routes);
