869a2fdac3b06e4de51e03a83f08a6c01cb4a577
Miguel Mejia <miguelmejiamontes@me.com>
2015-11-30 04:29:09 -0500

    finish routes... for now, implementing passport

--- cactus/routes/index.js
+++ cactus/routes/index.js
@@ -8,15 +8,59 @@ module.exports = function(app, passport) {
   // Initial login page
   app.get('/login', function(req, res) {
     //render the page and pass any existing flash data
-    res.render('login');
+    res.render('login', { message: req.flash('loginMessage')});
+  });
+
+  // process the login form
+  // note app.post, not app.get
+  app.post('/login', passport.authenticate('local-login', {
+    successRedirect : '/profile',
+    failureRedirect : '/login',
+    failureFlash    : true
+  }),
+      function(req, res) {
+        console.log("wow we made it this far");
+
+        if (req.body.remember) {
+          req.session.cookie.maxAge = 1000 * 60 * 10;
+        } else {
+          req.session.cookie.expires = false;
+        }
+      res.redirect('/');
   });
 
   app.get('/signup', function(req, res) {
-    res.render('signup');
+    res.render('signup', { message: req.flash('signupMessage') });
+  });
+
+  app.post('/signup', passport.authenticate('local-signup', {
+    successRedirect : '/profile', // redirect to the secure profile section
+    failureRedirect : '/signup', // redirect back to the signup page if there is an error
+    failureFlash : true // allow flash messages
+  }));
+
+  app.get('/profile', isLoggedIn, function(req, res) {
+    res.render('profile', {
+      username : req.user.username
+    });
   });
 
   app.get('/cook', function(req, res) {
     res.render('cook');
   });
 
+  app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+  });
 };
+
+function isLoggedIn(req, res, next) {
+
+	// if user is authenticated in the session, carry on
+	if (req.isAuthenticated())
+		return next();
+
+	// if they aren't redirect them to the home page
+	res.redirect('/');
+}
