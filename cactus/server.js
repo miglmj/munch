74b93f073c8ac301ab722c9d45382b33abd0c94a
Miguel Mejia <miguelmejiamontes@me.com>
2015-11-30 01:43:33 -0500

    server fix

--- cactus/server.js
+++ cactus/server.js
@@ -31,6 +31,9 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-app.listen(3000, function() {
-  console.log('App running! Hopefully...');
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('app listening at http://%s:%s', host, port);
 });
