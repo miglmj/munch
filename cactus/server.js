227b3ff53eb1f92a2c076975e75a264bae7452eb
Miguel Mejia <miguelmejiamontes@me.com>
2015-11-30 13:16:55 -0500

    try to fix server

--- cactus/server.js
+++ cactus/server.js
@@ -50,7 +50,7 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(8080, function () {
   var host = server.address().address;
   var port = server.address().port;
 
